2489c31c721ce434ccdec2d18e413316e0a828cd
Shaka Suzy <shaheedchambers@gmail.com>
2023-02-20 14:24:34 -0500

    Outlined comments in index and intern.js and manager.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Packages needed for application
+// Array of questions for user input
+// Function to generate HTML template literal
+// Function to write HTML file
+// Function to get manager data
+// Function to prompt next employee card
+// Function to get engineer data
+// Function to get intern data
+// Function call to initialize app
