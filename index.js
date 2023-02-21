5ba6d44659e3930802eae88ff9ac1060668cb25e
Shaka Suzy <shaheedchambers@gmail.com>
2023-02-21 14:07:02 -0500

    established required files

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Packages needed for application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const employeeList = [];
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 // Array of questions for user input
 // Function to generate HTML template literal
 // Function to write HTML file
