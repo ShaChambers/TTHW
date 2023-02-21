345b95187de7314aef04e20c3070a2b16f12f4bf
Shaka Suzy <shaheedchambers@gmail.com>
2023-02-21 14:10:58 -0500

    added manager questions and comments for following class questions

--- index.js
+++ index.js
@@ -7,6 +7,32 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 // Array of questions for user input
+const managerQuestions = [
+  {
+    type: "input",
+    message: "Enter manager's name:",
+    name: "managerName",
+  },
+  {
+    type: "input",
+    message: "Enter manager's employee ID:",
+    name: "managerId",
+  },
+  {
+    type: "input",
+    message: "Enter manager's email address:",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "Enter manager's office number:",
+    name: "officeNumber",
+  },
+];
+//engineer
+//intern
+//checkpoint
+
 // Function to generate HTML template literal
 // Function to write HTML file
 // Function to get manager data
